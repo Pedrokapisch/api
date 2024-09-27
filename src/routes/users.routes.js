b2583c74c732367377f930146ae159681d92a486
pedro <pedro-gugusto@hotmail.com>
2024-09-27 18:17:23 -0300

    created middleware

---
+++ src/routes/users.routes.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const UsersController = require("../controllers/UsersController");
+
+const usersRoutes = Router();
+
+function myMiddleware(request, response, next) {
+  
+}
+
+
+
+
+const usersController = new UsersController();
+
+usersRoutes.post("/", usersController.create);
+
+module.exports = usersRoutes;
