e7463f92f4d061745fc0a62bb199cea0ff8c36c9
pedro <pedro-gugusto@hotmail.com>
2024-10-14 19:50:27 -0300

    Created an way to make a table and capture requests to create an line

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,15 +1,27 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    if (!name) {
-      throw new AppError("O nome é obrigatório");
-    }
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
   
-    response.status(201).json({ name, email, password });
+    if(checkUserExists) {
+      throw new AppError('Este e-mail já esta em uso')
+    };
+
+    const hashedPassword = await hash(password, 8 /*salt?(8) é o fator de complexidade*/);
+    
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",[ name, email, hashedPassword]
+    );
+
+    return response.status(201).json();
   }
+  
 }
 
 module.exports = UsersController
